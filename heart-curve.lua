c6560699f7ca05d02a07d3cf4709fb7e1717871b
Tony Schiffbauer <tonyrocksnow@gmail.com>
2014-05-30 10:51:48 -0400

    Removed Commodore Basic 7 draw references

--- heart-curve.lua
+++ heart-curve.lua
@@ -6,12 +6,8 @@ local y1 = math.sqrt(x*x) + q
 local xa = ((x+1)*160)
 local ya = ((y0+1)*100)
 local yb = ((y1-1)*100)
---50 draw 1,xa,200-ya
+
 print(xa,200-ya)
-
---52 draw 1,xa,40-yb
 print(xa,40-yb)
-
---58 print yb
 print(yb)
 end
