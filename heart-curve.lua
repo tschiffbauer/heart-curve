c84dcf8d230b98fc674bc9085a54b3e3995474b0
Tony Schiffbauer <tonyrocksnow@gmail.com>
2014-05-30 22:43:34 -0400

    Added Roblox code
    Added the code to build the Roblox objects.

--- heart-curve.lua
+++ heart-curve.lua
@@ -1,4 +1,3 @@
-
 for x = -1,1,.01 do
 local q  = math.sqrt(1-x*x)
 local y0 = math.sqrt(x*x) - q
@@ -7,7 +6,16 @@ local xa = ((x+1)*160)
 local ya = ((y0+1)*100)
 local yb = ((y1-1)*100)
 
-print(xa,200-ya)
-print(xa,40-yb)
-print(yb)
+local brick = Instance.new("Part", game.Workspace)
+brick.Anchored  = true
+brick.Name = x.."NewBrick"
+brick.Size = Vector3.new(1, 1, 1)
+brick.Position = Vector3.new(xa,200-ya,0)
+
+local brick = Instance.new("Part", game.Workspace)
+brick.Anchored  = true
+brick.Name = x.."NewBrick"
+brick.Size = Vector3.new(1, 1, 1)
+brick.Position = Vector3.new(xa,40-yb,0)
+
 end
